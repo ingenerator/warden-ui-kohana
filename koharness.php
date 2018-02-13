9efc4c9c1aeafebed5d463b9577b0d368684153d
Andrew Coulton <andrew@ingenerator.com>
2018-02-13 17:05:18 +0000

    Add basic build requirements

---
+++ koharness.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'modules' => [
+        'warden'      => __DIR__,
+        'kohana-view' => __DIR__.'/vendor/ingenerator/kohana-view',
+    ],
+];
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,14 +1,12 @@
 <?php
-/**
- * @author    Andrew Coulton <andrew@ingenerator.com>
- * @copyright 2015 inGenerator Ltd
- * @licence   proprietary
- */
 $_SERVER['KOHANA_ENV'] = 'DEVELOPMENT';
-require_once(__DIR__.'/../../../application/bootstrap.php');
+require_once __DIR__.'/../koharness_bootstrap.php';
 
-// And autoload mocks etc for other bits
-require_once(__DIR__.'/../../../vendor/ingenerator/warden-core/test/bootstrap.php');
+// Require in warden core test bootstrap to autoload mocks and base tests
+require_once __DIR__.'/../vendor/ingenerator/warden-core/test/bootstrap.php';
+
+// Require dummy page layout view
+require_once __DIR__.'/../vendor/ingenerator/kohana-view/tests/mock/ViewModel/PageLayout/DummyPageLayoutView.php';
 
 // Autoload mocks and test-support helpers that should not autoload in the main app
 $mock_loader = new \Composer\Autoload\ClassLoader;
